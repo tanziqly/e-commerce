32181f67e260f2f2afe89d8ec2c90f773a9eccaa
tanziqly <lhonston@gmail.com>
2024-10-29 21:41:47 +0300

    auth

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,9 +2,10 @@ const Router = require("express");
 const router = new Router();
 
 const UserController = require("../controllers/UserController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/registration", UserController.registration);
 router.post("/login", UserController.login);
-router.get("/auth", UserController.isAuth);
+router.get("/auth", authMiddleware, UserController.isAuth);
 
 module.exports = router;
